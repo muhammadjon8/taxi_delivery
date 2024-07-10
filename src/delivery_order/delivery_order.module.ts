1a7578211bebfcde1ed3f12cf63c390118d6811b
muhammadjon8 <umuhammadjon22@gmail.com>
2024-07-10 12:57:23 +0500

    DeliveryOrderService

--- src/delivery_order/delivery_order.module.ts
+++ src/delivery_order/delivery_order.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DeliveryOrderService } from './delivery_order.service';
 import { DeliveryOrderController } from './delivery_order.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DeliveryOrder } from './entities/delivery_order.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DeliveryOrder])],
   controllers: [DeliveryOrderController],
   providers: [DeliveryOrderService],
 })
