6a75cae50857310e6d6f44b7a22c10da7e5ae4a2
Bekzod <bekzodtoxtamuratov@gmail.com>
2024-07-10 22:40:56 +0300

    Car tugadi

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CarModule } from './car/car.module';
 import { Car } from './car/entities/car.entity';
 import { DeliveryOrderModule } from './delivery_order/delivery_order.module';
+import { DriverModule } from './driver/driver.module';
+import { Driver } from './driver/entities/driver.entity'
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
@@ -14,12 +16,13 @@ import { DeliveryOrderModule } from './delivery_order/delivery_order.module';
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
-      entities: [Car],
+      entities: [Car,Driver],
       synchronize: true,
       logging: false,
     }),
     DeliveryOrderModule,
     CarModule,
+    DriverModule,
   ],
   controllers: [],
   providers: [],
