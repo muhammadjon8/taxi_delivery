fbbecdf94b576ca588efdbbf980e7496870a0cc0
Bekzod <bekzodtoxtamuratov@gmail.com>
2024-07-11 09:49:42 +0300

    123

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CarModule } from './car/car.module';
-import { Car } from './car/entities/car.entity';
 import { DeliveryOrderModule } from './delivery_order/delivery_order.module';
 import { DriverModule } from './driver/driver.module';
-import { Driver } from './driver/entities/driver.entity'
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
@@ -16,7 +14,7 @@ import { Driver } from './driver/entities/driver.entity'
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
-      entities: [Car,Driver],
+      entities: [],
       synchronize: true,
       logging: false,
     }),
