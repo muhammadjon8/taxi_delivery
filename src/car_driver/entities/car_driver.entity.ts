6da290a4f7a6539ffc1cfbc081756f13b43c5d5d
Bekzod <bekzodtoxtamuratov@gmail.com>
2024-07-12 15:07:04 +0300

    12

--- src/car_driver/entities/car_driver.entity.ts
+++ src/car_driver/entities/car_driver.entity.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity('car_driver')
 export class CarDriver {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: 'driver_id', description: 'driver_example' })
   @Column()
   driver_id: number;
 
+  @ApiProperty({ example: 'car_id example', description: 'car_id example' })
   @Column()
   car_id: number;
 }
