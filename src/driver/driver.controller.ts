a1d07af61afc626e486d0c8b2ebbd139c4488d40
blackseriesC6 <blackseries.n63@gmail.com>
2024-08-04 11:37:25 +0500

    "register qohsildi

--- src/driver/driver.controller.ts
+++ src/driver/driver.controller.ts
@@ -17,7 +17,7 @@ import { Driver } from './entities/driver.entity';
 export class DriverController {
   constructor(private readonly driverService: DriverService) {}
 
-  @ApiOperation({ summary: 'bu yerda post qilinadi' })
+  @ApiOperation({ summary: 'bu yerda post qilinadi shunaqa' })
   @ApiResponse({
     status: 201,
     description: 'The post created.',
